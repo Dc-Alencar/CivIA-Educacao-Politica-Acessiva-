b1d3d2d3ce23b835c8a652fac1481127870c13b4
Call333 <cam4@aluno.ifal.edu.br>
2026-05-23 11:15:14 -0300

    fazendo pequenos ajustes e criando a claasse userprogress.py

---
+++ backend/content/models/UserProgress.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+
+from .topic import Topic
+
+print(settings)
+
+class UserProgress(models.Model):
+    STATUS_CHOICES = [
+        ('NOT_STARTED', 'Not started'),
+        ('IN_PROGRESS', 'In progress'),
+        ('DONE', 'Done')
+    ]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="progress"
+    )
+
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="progress"
+    )
+
+    status = models.ForeignKey(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="NOT_STARTED"
+    )
+
+    completed_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    last_acessed_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("user", "topic")
