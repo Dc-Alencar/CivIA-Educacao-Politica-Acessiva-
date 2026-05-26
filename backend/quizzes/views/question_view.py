45c2c172442d5a3c0e1fa84e4794f71fcfa5111b
Call333 <cam4@aluno.ifal.edu.br>
2026-05-26 10:45:23 -0300

    colocando parâmetros no endpoint /api/quizzes/questions/?module_order=?&topic_order=?

--- backend/quizzes/views/question_view.py
+++ backend/quizzes/views/question_view.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from quizzes.models import Question
+
+from content.models import Topic
+
 from quizzes.serializers import QuestionSerializer
 
 class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
@@ -11,15 +14,27 @@ class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        
+
         queryset = Question.objects.all()
 
-        topic_id = self.request.query_params.get("topic")
+        module_order = self.request.query_params.get(
+            "module_order"
+        )
 
-        if topic_id:
+        topic_order = self.request.query_params.get(
+            "topic_order"
+        )
+
+        if module_order:
 
             queryset = queryset.filter(
-                topic_id=topic_id
+                topic__module__order=module_order
             )
 
-        return queryset
+        if topic_order:
+
+            queryset = queryset.filter(
+                topic__order=topic_order
+            )
+
+        return queryset.order_by("id")
