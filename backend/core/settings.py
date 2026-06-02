77841947b774f842f5ab51b4947d6b9a15393045
MWillian <matheuswno02@gmail.com>
2026-06-02 12:06:59 -0300

    Implementação do botão de logout, e ajuste no cors.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'quizzes',
     'ai_assistant',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5000",
+    "http://127.0.0.1:5000",
 ]
 
 ROOT_URLCONF = 'core.urls'
